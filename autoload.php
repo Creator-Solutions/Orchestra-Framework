30dc2415e328e18c4e77b0614c56c00c877264be
Owen Burns <owenburns39@gmail.com>
2023-10-14 14:59:31 +0200

    Initial Commit

---
+++ autoload.php
@@ -0,0 +1,27 @@
+<?php
+
+define('PHPNEXUS_VERSION', '0.0.1');
+
+$classmap = [
+    'Orchestra' => __DIR__ . '/Orchestra/',
+];
+
+spl_autoload_register(function(string $classname) use ($classmap) {
+    $parts = explode('\\', $classname);
+
+    $namespace = array_shift($parts);
+    $classfile = array_pop($parts) . '.php';
+
+    if (! array_key_exists($namespace, $classmap)) {
+        return;
+    }
+
+    $path = implode(DIRECTORY_SEPARATOR, $parts);
+    $file = $classmap[$namespace] . $path . DIRECTORY_SEPARATOR . $classfile;
+
+    if (! file_exists($file) && ! class_exists($classname)) {
+        return;
+    }
+
+    require_once $file;
+});
