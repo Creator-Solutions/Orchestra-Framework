1535baf67e0523f4c6207760f8deb333148d96a1
Creator-Solutions <owenburns39@gmail.com>
2025-03-13 22:39:36 +0200

    Implemented a code change test

--- app/resources/views/welcome.pulse.php
+++ app/resources/views/welcome.pulse.php
@@ -18,7 +18,7 @@
     <main>
       <p>To get started, you can:</p>
       <ul>
-        <li>Check out the <a href="https://github.com/creator-solutions/Orchestra-Framework">documentation</a></li>
+        <li>Check out the <a href="https://github.com/creator-solutions/Orchestra-Framework">Repository</a></li>
         <li>Explore the <a href="https://github.com/creator-solutions/Orchestra-Framework">project's source code</a>
         </li>
         <li>Modify the configuration files in <code>.env</code></li>
